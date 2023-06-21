db20d221b4b084d26e4a67b8a3a5d0f4f3ed68b0
Patricio Uskaer <patriciouskaer@gmail.com>
2023-06-20 23:28:15 -0300

    year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row space-y-6 lg:space-y-0 items-center justify-between'>
           <p className='text-paragraph opacity-80 text-[15px]'>
-            &copy; 2022 Patricio Uskaer. All rights reserved.
+            &copy; {new Date().getFullYear()} - Patuka Technologies, all rights reserved.
           </p>
         </div>
       </div>
